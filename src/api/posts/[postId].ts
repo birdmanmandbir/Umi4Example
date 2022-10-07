61223c4191473e0b825e95e4cbdd5d6f197156f5
Neil <1522674529@qq.com>
2022-10-07 09:57:43 +0800

    api/[postId]

--- src/api/posts/[postId].ts
+++ src/api/posts/[postId].ts
@@ -1,5 +1,33 @@
-import type { UmiApiRequest, UmiApiResponse } from "umi";
- 
+import type { UmiApiRequest, UmiApiResponse } from 'umi';
+import { PrismaClient } from '@prisma/client';
+
 export default async function (req: UmiApiRequest, res: UmiApiResponse) {
-  res.status(400).json({ error: "This API is not implemented yet." })
+  switch (req.method) {
+    // 查询文章
+    case 'GET':
+      try {
+        const prisma = new PrismaClient();
+        const post = prisma.post.findUnique({ include: { author: true }, where: { id: req.params.postId } });
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({
+            error: 'Page not found.',
+          });
+        }
+        await prisma.$disconnect();
+        break;
+      } catch (e: any) {
+        res.status(500).json({
+          result: false,
+          message:
+            typeof e.code === 'string'
+              ? 'https://www.prisma.io/docs/reference/api-reference/error-reference#' + e.code.toLowerCase()
+              : e,
+        });
+      }
+    default:
+      // 如果不是 POST 请求，代表他正在用错误的方式访问这个 API
+      res.status(405).json({ error: 'Method not allowed' });
+  }
 }
